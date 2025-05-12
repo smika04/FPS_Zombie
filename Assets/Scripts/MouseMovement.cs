5d1ec7fdeb5360e9f76ac130ec483620f3305566
SeverynUA <cool.muzzy.gmaio.com@gmail.com>
2025-05-11 18:33:12 -0700

    Add basic physic bullets

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -4,31 +4,34 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-    public float mouseSensitivity = 500f;
+    public float mouseSensitivity = 100f;    
 
-    float xRotation = 0f;
-    public float yRotation = 0f;
+    float xRotation = 0f;                   
+    public float yRotation = 0f;           
 
-    public float topClamp = -90f;
-    public float bottomClamp = 90f;
+    public float topClamp = -90f;            
+    public float bottomClamp = 90f;         
 
     void Start()
     {
+        // Ховаємо курсор миші та блокуємо його в центрі екрану
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Отримуємо рух миші
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        // Обчислюємо поворот по X (вверх-вниз)
         xRotation -= mouseY;
-
         xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
 
+        // Обчислюємо поворот по Y (вліво-вправо)
         yRotation += mouseX;
 
+        // Застосовуємо обертання до об'єкта (ймовірно, камери)
         transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -29,5 +29,13 @@ public class Weapon : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab,bulletSpawn.position, Quaternion.identity);
 
         bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+       yield return new WaitForSeconds(delay);
+       Destroy(bullet);
     }
 }
