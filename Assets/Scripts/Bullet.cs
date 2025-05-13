7262edd3954c2e9032fd8ff094401ab10bab931f
SeverynUA <cool.muzzy.gmaio.com@gmail.com>
2025-05-12 17:58:40 -0700

    added bullet effects

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -11,14 +11,37 @@ public class Bullet : MonoBehaviour
         if (collision.gameObject.CompareTag("Target"))
         {
             print("hit " + collision.gameObject.name + "!");
+
+            CreateBulletImpactEffect(collision);
+
             // Знищуємо кулю після попадання
             Destroy(gameObject);
         }
         if (collision.gameObject.CompareTag("Wall"))
         {
             print("hit " + collision.gameObject.name + "!");
+
+            CreateBulletImpactEffect(collision);
+
             // Знищуємо кулю після попадання
             Destroy(gameObject);
         }
+    }
+
+    // Метод для створення ефекту влучення кулі по об'єкту
+    void CreateBulletImpactEffect(Collision objectHit)
+    {
+        // Отримуємо першу точку контакту при зіткненні
+        ContactPoint contact = objectHit.contacts[0];
+
+        // Створюємо ефект (наприклад, слід кулі) у точці контакту
+        GameObject hole = Instantiate(
+            GlobalReferences.Instance.bulletImpactEffectPrefab, // беремо префаб з глобального посилання
+            contact.point,                                      // координати точки зіткнення
+            Quaternion.LookRotation(contact.normal)            // орієнтація ефекту за нормаллю поверхні
+        );
+
+        // Прив'язуємо створений об'єкт до об'єкта, в який влучила куля
+        hole.transform.SetParent(objectHit.gameObject.transform);
     }
 }
